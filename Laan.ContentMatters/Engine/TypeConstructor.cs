cf833f501b49b5026baa8142c43234e1592b1fb7
benlaan <benlaan@d708858c-815c-11de-a6cd-7b297ead3933>
2009-08-09 11:52:45 +0000

    Now passing some tests for TypeConstructor

--- Laan.ContentMatters/Engine/TypeConstructor.cs
+++ Laan.ContentMatters/Engine/TypeConstructor.cs
@@ -2,18 +2,56 @@ using System;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Collections.Generic;
+using System.IO;
 
 using Laan.ContentMatters.Models;
+using Laan.Utilities.IO;
 
 namespace Laan.ContentMatters.Engine
 {
     public class TypeConstructor
     {
-        private static MethodBuilder GetGetter( TypeBuilder tb, string fieldName, FieldBuilder accessField )
-        {
-            var getter = tb.DefineMethod( "get" + fieldName, /*MethodAttributes.Virtual |*/ MethodAttributes.Public );
-            var gen = getter.GetILGenerator();
+        private AssemblyBuilder _assemblyBuilder;
+        private string _fileName;
+        private TypeBuilder _builder;
+        private ModuleBuilder _moduleBuilder;
 
+        private MethodAttributes MethodAttributes =
+            MethodAttributes.Public |
+            MethodAttributes.Virtual |
+            MethodAttributes.HideBySig |
+            MethodAttributes.NewSlot |
+            MethodAttributes.SpecialName;
+
+        public TypeConstructor( string @namespace )
+            : this( @namespace, null )
+        {
+        }
+
+        public TypeConstructor( string @namespace, string path )
+        {
+            _fileName = @namespace + ".dll";
+            var access = path != null ? AssemblyBuilderAccess.RunAndSave : AssemblyBuilderAccess.Run;
+            path = !String.IsNullOrEmpty( path ) ? Path.GetDirectoryName( path ) : null;
+
+            _assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly( new AssemblyName( @namespace ), access, path );
+
+            if ( !String.IsNullOrEmpty( path ) )
+                _moduleBuilder = _assemblyBuilder.DefineDynamicModule( @namespace, _fileName );
+            else
+                _moduleBuilder = _assemblyBuilder.DefineDynamicModule( @namespace );
+        }
+
+        private FieldBuilder CreateField( TypeBuilder tb, Type fieldType, string fieldName )
+        {
+            return tb.DefineField( fieldName, fieldType, FieldAttributes.Private );
+        }
+
+        private MethodBuilder CreateGetter( FieldBuilder accessField )
+        {
+            var getter = _builder.DefineMethod( "get" + accessField.Name, MethodAttributes, accessField.FieldType, Type.EmptyTypes );
+
+            var gen = getter.GetILGenerator();
             gen.Emit( OpCodes.Ldarg_0 );
             gen.Emit( OpCodes.Ldfld, accessField );
             gen.Emit( OpCodes.Ret );
@@ -21,66 +59,55 @@ namespace Laan.ContentMatters.Engine
             return getter;
         }
 
-        private static MethodBuilder GetSetter( TypeBuilder tb, string fieldName, FieldBuilder accessField )
+        private MethodBuilder CreateSetter( FieldBuilder field )
         {
-            var setter = tb.DefineMethod( "set" + fieldName, /*MethodAttributes.Virtual |*/ MethodAttributes.Public );
+            var setter = _builder.DefineMethod( "set" + field.Name, MethodAttributes, null, new[] { field.FieldType } );
 
             var gen = setter.GetILGenerator();
             gen.Emit( OpCodes.Ldarg_0 );
             gen.Emit( OpCodes.Ldarg_1 );
-            gen.Emit( OpCodes.Stfld, accessField );
+            gen.Emit( OpCodes.Stfld, field );
             gen.Emit( OpCodes.Ret );
 
             return setter;
         }
 
-        private static FieldBuilder GetAccessField( TypeBuilder tb, Type fieldType, string fieldName )
+        private void CreateProperty( FieldDefinition definition )
         {
-            return tb.DefineField( fieldName, fieldType, FieldAttributes.Private );
+            var fieldType = definition.ToSystemType();
+            var fieldName = String.Format( "_{0}{1}", definition.Name.Substring( 0, 1 ).ToLower(), definition.Name.Substring( 1 ) );
+
+            // backing field
+            var accessField = CreateField( _builder, fieldType, fieldName );
+
+            // accessors
+            var getter = CreateGetter( accessField );
+            var setter = CreateSetter( accessField );
+
+            // property, with linked accessors
+            var property = _builder.DefineProperty( definition.Name, PropertyAttributes.None, fieldType, null );
+            property.SetGetMethod( getter );
+            property.SetSetMethod( setter );
         }
 
-        public static Type Create( Laan.ContentMatters.Models.ItemDefinition def )
+        public Type AddType( Type baseType, ItemDefinition itemDefinition )
         {
-            AppDomain domain = AppDomain.CurrentDomain;
-
-            AssemblyBuilder ab = domain.DefineDynamicAssembly
-            (
-                new AssemblyName( def.Namespace ),
-                AssemblyBuilderAccess.Run
-            );
-
-            // create a new module to hold code in the assembly
-            ModuleBuilder mb = ab.DefineDynamicModule( def.Namespace );
-
             var attr =
-                TypeAttributes.Public | TypeAttributes.Class |
-                TypeAttributes.AutoClass | TypeAttributes.AnsiClass |
-                TypeAttributes.BeforeFieldInit | TypeAttributes.AutoLayout;
+                TypeAttributes.Public | TypeAttributes.Class | TypeAttributes.AutoClass |
+                TypeAttributes.AnsiClass | TypeAttributes.BeforeFieldInit | TypeAttributes.AutoLayout;
 
             // create a type in the module
-            TypeBuilder tb = mb.DefineType( def.Name, attr, typeof( Item ) );
+            _builder = _moduleBuilder.DefineType( String.Format( "{0}.{1}", itemDefinition.Namespace, itemDefinition.Name ), attr, baseType );
 
             // build the properties
-            foreach ( var field in def.Fields )
-            {
-                Type fieldType = field.ToSystemType();
+            itemDefinition.Fields.ForEach( CreateProperty );
 
-                string fieldName = String.Format(
-                    "_{0}{1}",
-                    field.Name.Substring( 0, 1 ).ToLower(), field.Name.Substring( 1 )
-                );
+            return _builder.CreateType();
+        }
 
-                var accessField = GetAccessField( tb, fieldType, fieldName );
-                var getter = GetGetter( tb, fieldName, accessField );
-                var setter = GetSetter( tb, fieldName, accessField );
-
-                var property = tb.DefineProperty( fieldName, PropertyAttributes.None, fieldType, null );
-                property.SetGetMethod( getter );
-                property.SetSetMethod( setter );
-            }
-
-            // finish creating the type and make it available
-            return tb.CreateType();
+        public void SaveAssembly()
+        {
+            _assemblyBuilder.Save( _fileName );
         }
     }
 }
