6340b6ba189252313adb8d319dde3d00ac7d7bfa
benlaan <benlaan@d708858c-815c-11de-a6cd-7b297ead3933>
2010-04-13 12:58:39 +0000

    Add: DataDictionary with Tests to allow access to items by property (including sub properties)

---
+++ trunk/Laan.ContentMatters/Engine/Interfaces/IDataDictionary.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace Laan.ContentMatters.Engine.Interfaces
+{
+    public interface IDataDictionary
+    {
+        void Add( string key, object value );
+        bool Remove( string key );
+        ICollection<string> Keys { get; }
+        ICollection<object> Values { get; }
+        object this[ string key ] { get; set; }
+        bool TryGetValue( string key, out object value );
+    }
+}
