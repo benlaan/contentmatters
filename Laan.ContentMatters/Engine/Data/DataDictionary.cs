7a253a885770fbf4fbaef90dfab6c1379e24db4e
benlaan <benlaan@d708858c-815c-11de-a6cd-7b297ead3933>
2010-04-13 12:58:39 +0000

    Add: DataDictionary with Tests to allow access to items by property (including sub properties)

---
+++ Laan.ContentMatters/Engine/Data/DataDictionary.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+
+using Laan.ContentMatters.Engine.Interfaces;
+
+namespace Laan.ContentMatters.Engine.Data
+{
+    public class DataDictionary : IDataDictionary, IEnumerable<object>
+    {
+        Dictionary<string, object> _data;
+
+        public DataDictionary()
+        {
+            _data = new Dictionary<string, object>();
+        }
+
+        private object GetData( string key )
+        {
+            object instance;
+            if ( _data.TryGetValue( key, out instance ) )
+                return instance;
+
+            string[] parts = key.Split( new[] { '.' } );
+
+            int index = 0;
+            if ( !_data.TryGetValue( parts[ index ], out instance ) )
+                throw new ArgumentException( String.Format(" Data not found for key: {0}", key ) );
+
+            index++;
+            while ( index < parts.Length )
+            {
+                Type t = instance.GetType();
+                var prop = t.GetProperty( parts[ index ] );
+
+                instance = prop.GetValue( instance, null );
+                index++;
+            }
+
+            return instance;
+        }
+
+        #region IDictionary<string,object> Members
+
+        public void Add( string key, object value )
+        {
+            _data.Add( key, value );
+        }
+
+        public bool Remove( string key )
+        {
+            return _data.Remove( key );
+        }
+
+        public ICollection<string> Keys
+        {
+            get { return _data.Keys; }
+        }
+        
+        public ICollection<object> Values
+        {
+            get { return _data.Values; }
+        }
+        
+        public object this[ string key ]
+        {
+            get { return GetData( key ); }
+            set { _data[ key ] = value; }
+        }
+
+        public bool TryGetValue( string key, out object value )
+        {
+            value = GetData(key);
+            return ( value != null );
+        }
+
+        #endregion
+
+        #region IEnumerable<object> Members
+
+        //public IEnumerator<object> GetEnumerator()
+        //{
+        //    return _data.GetEnumerator();
+        //}
+
+        #endregion
+
+        #region IEnumerable Members
+
+        //System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        #endregion
+    }
+}
